0be208915361102dde4fdcc396809c97daf7c538
Yohan Gonzalez Almaguer <yohan.gonzalez.almaguer@gmail.com>
2020-04-28 21:40:12 -0400

    Favorite Video Page Complete

--- src/app/modules/shared/components/favorite-table/favorite-table.component.ts
+++ src/app/modules/shared/components/favorite-table/favorite-table.component.ts
@@ -3,7 +3,7 @@ import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 import {animate, state, style, transition, trigger} from '@angular/animations';
-import {faGamepad, faThumbsUp, faEye, faComment} from '@fortawesome/free-solid-svg-icons';
+import {faGamepad, faThumbsUp, faEye, faComment, faStar, faFilter} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-favorite-table',
@@ -11,33 +11,49 @@ import {faGamepad, faThumbsUp, faEye, faComment} from '@fortawesome/free-solid-s
   styleUrls: ['./favorite-table.component.scss'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('collapsed', style({height: '0', minHeight: '0'})),
       state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)'))
     ])
   ]
 })
 export class FavoriteTableComponent implements OnInit {
 
+  faStar = faStar;
   faGamepad = faGamepad;
   faThumbsUp = faThumbsUp;
   faEye = faEye;
   faComment = faComment;
+  faFilter = faFilter;
 
   columnsToDisplay = ['categoria', 'titulo', 'propietario', 'fecha'];
   expandedElement: PeriodicElement | null;
-  dataSource: MatTableDataSource<PeriodicElement>;
 
+  dataSource: MatTableDataSource<PeriodicElement>;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
+  show: boolean;
+  filterForm: JQuery<HTMLElement>;
 
   constructor() {
+    this.show = false;
     this.dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   }
 
   ngOnInit() {
+    this.filterForm = $('#filterForm');
+    this.filterForm.toggle(0);
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  toggleFilterForm() {
+    this.filterForm.toggle(750);
   }
 }
 
