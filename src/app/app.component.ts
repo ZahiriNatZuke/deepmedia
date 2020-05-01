81cc9a1d8902c5bbab6cee81b2d33c4edea7a0d4
Yohan Gonzalez Almaguer <yohan.gonzalez.almaguer@gmail.com>
2020-05-01 19:39:39 -0400

    Denied Access from iOS & Android

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Platform} from '@angular/cdk/platform';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  checkPlatform: any;
+
+  constructor(public platform: Platform, private routerActive: Router) {
+    this.checkPlatform = setInterval(() => {
+      if (this.platform.ANDROID || this.platform.IOS)
+        this.routerActive.navigate(['/forbidden']).then();
+    }, 50);
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.checkPlatform);
   }
 }
