d33c17464dc92e2843a77fc66ec451752a4ba523
Yohan Gonzalez Almaguer <yohan.gonzalez.almaguer@gmail.com>
2020-05-22 22:59:58 -0400

    Videos by Categories with Paginate

--- src/app/modules/video/video-list/video-list.component.ts
+++ src/app/modules/video/video-list/video-list.component.ts
@@ -3,6 +3,7 @@ import {CrudService} from '../../../services/crud.service';
 import {API} from '../../../services/API';
 import {ActivatedRoute} from '@angular/router';
 import {Video} from '../../../models/video';
+import {faAngleDown} from '@fortawesome/free-solid-svg-icons';
 
 const api = new API();
 
@@ -12,30 +13,48 @@ const api = new API();
   styleUrls: ['./video-list.component.scss']
 })
 export class VideoListComponent implements OnInit {
-  public videos: Video[];
+  videos: Video[];
   progressBar: JQuery<HTMLElement>;
   cardColumn: JQuery<HTMLElement>;
+  next_page_url: string;
+  faAngleDown = faAngleDown;
+  loading: boolean;
 
-  constructor(private crudService: CrudService, private activatedRoute: ActivatedRoute) {
+  constructor(private crudService: CrudService,
+              private activatedRoute: ActivatedRoute) {
+    this.loading = false;
+    this.activatedRoute.params.subscribe(params => {
+      const category: string = params.category;
+      this.crudService.GETWithOutAuth(api.getVideosByCategoryURL(), category)
+        .subscribe(response => {
+          this.next_page_url = response.videos.next_page_url;
+          this.videos = response.videos.data;
+          this.stopLoading();
+        });
+    });
   }
 
   ngOnInit(): void {
     this.progressBar = $('mat-progress-bar');
     this.cardColumn = $('.card-columns');
     this.cardColumn.hide(0);
-    this.activatedRoute.params.subscribe(params => {
-      const category: string = params.category;
-      this.crudService.GETWithOutAuth(api.getVideosByCategoryURL(), category)
-        .subscribe(response => {
-          this.videos = response.videos;
-          this.stopLoading();
-        });
-    });
   }
 
   stopLoading() {
     this.progressBar.slideUp(400);
     this.cardColumn.fadeIn(650);
+  }
+
+  fetchMoreVideos() {
+    this.loading = true;
+    this.crudService.GETWithOutAuth(this.next_page_url).subscribe(response => {
+      setTimeout(() => {
+        const newVideos: Video[] = response.videos.data;
+        this.videos.push(...newVideos);
+        this.next_page_url = response.videos.next_page_url;
+        this.loading = false;
+      }, 500);
+    });
   }
 
 }
